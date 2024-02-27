3d77a65a6206e1f5fa4f8b7fc1f84268fea39b84
Fatmaud <fatmaudemirci@gmail.com>
2024-02-27 15:07:59 +0100

    events added

---
+++ Class_Notes/RC-05-Events-Hooks/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+//import Events from "./1-events-hookIntro/Events";
+import Hooks from "./3-hooks/Hooks";
+import Counter from "./classComponents/Counter";
+
+const App = () => {
+  return (
+    <div>
+      {/* <Events /> */}
+      {/* <Hooks /> */}
+      <Counter />
+    </div>
+  );
+};
+
+export default App;
