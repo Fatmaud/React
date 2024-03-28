2cead690e724a322f2bf2abe639c91cbe55b7e76
Fatmaud <fatmaudemirci@gmail.com>
2024-03-28 11:41:42 +0100

    recipe app first commit

---
+++ Class_Notes/RC-17-18-RecipeAppProject/src/pages/home/RecipeCard.jsx
@@ -0,0 +1,27 @@
+import React, { useContext } from "react";
+import {
+  Button,
+  Cards,
+  MainContainer,
+  RecipeHeader,
+  RecipeImage,
+} from "./HomeStyles";
+import { RecipeContext } from "../../context/RecipeProvider";
+
+const RecipeCard = () => {
+  const { recipes } = useContext(RecipeContext);
+  return (
+    <MainContainer>
+      {recipes.map((a) => (
+        <Cards key={a.recipe.id}>
+          <RecipeHeader>{a.recipe.label}</RecipeHeader>
+
+          <RecipeImage src={a.recipe.image} />
+          <Button>Details</Button>
+        </Cards>
+      ))}
+    </MainContainer>
+  );
+};
+
+export default RecipeCard;
